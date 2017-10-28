7a400c81c4c831cfef73434bdf560ee948172e96
Nicolas Schmitt <nicolas.schmitt@outlook.fr>
2017-10-28 20:38:42 +0200

    first

---
+++ UserInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DracoApi.Net
+{
+    public class UserInfo
+    {
+        public string Nickname { get; set; }
+        public string DeviceId { get; set; }
+        public string DeviceAdId { get; set; }
+        public string UserId { get; set; }
+        public string PortalId { get; set; }
+        public int Avatar { get; set; }
+    }
+}
