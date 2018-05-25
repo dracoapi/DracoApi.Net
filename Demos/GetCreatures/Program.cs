bbb5c88c589156a2b542f67ce5452a5208810705
Furtif <msntchat@hotmail.fr>
2018-05-25 21:48:23 +0200

    add missed

--- Demos/GetCreatures/Program.cs
+++ Demos/GetCreatures/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DracoLib.Core;
+using DracoProtos.Core.Config;
+using DracoProtos.Core.Enums;
+using DracoProtos.Core.Objects;
+using System;
+using System.Collections.Generic;
 
 namespace GetCreatures
 {
@@ -6,7 +11,42 @@ namespace GetCreatures
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+
+            Console.WriteLine("Creating new Configuration...");
+            User config = new User()
+            {
+                Username = "xxxxxxx@gmail.com",
+                Password = "xxxxxxx",
+                DeviceId = "xxxxxxx-xxxxxxx-xxxxxxx-xxxxxxx-xxxxxxx",
+                Login = "GOOGLE"
+            };
+
+            var draco = new DracoClient("http://localhost:8888");
+
+            Console.WriteLine("Ping...");
+            draco.Ping();
+
+            Console.WriteLine("Boot...");
+            draco.Boot(config);
+
+            Console.WriteLine("Login...");
+            draco.Login();
+
+            Console.WriteLine("Init client...");
+            draco.Load();
+
+            Console.WriteLine("Get creatures...");
+            var response = draco.Inventory.GetUserCreatures();
+            //for (const creature of response.userCreatures) {
+            //    const name = creature.alias || strings.getCreature(DracoNode.enums.CreatureType[creature.name]);
+            //    Console.WriteLine(`  ${name
+            //}
+            //lvl ${creature.leve}
+            //          , cp=${creature.cp}`);
+            // }
+
+            Console.WriteLine("Done.");
         }
     }
 }
--- Demos/GetItems/Program.cs
+++ Demos/GetItems/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using DracoLib.Core;
+using DracoProtos.Core.Config;
+using DracoProtos.Core.Enums;
+using DracoProtos.Core.Objects;
+using System;
+using System.Collections.Generic;
 
 namespace GetItems
 {
@@ -6,7 +11,39 @@ namespace GetItems
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+
+            Console.WriteLine("Creating new Configuration...");
+            User config = new User()
+            {
+                Username = "xxxxxxx@gmail.com",
+                Password = "xxxxxxx",
+                DeviceId = "xxxxxxx-xxxxxxx-xxxxxxx-xxxxxxx-xxxxxxx",
+                Login = "GOOGLE"
+            };
+
+            var draco = new DracoClient(/*"http://localhost:8888"*/);
+
+            Console.WriteLine("Ping...");
+            var ping = draco.Ping();
+            if (!ping) throw new Exception();
+
+            Console.WriteLine("Boot...");
+            draco.Boot(config);
+
+            Console.WriteLine("Login...");
+            draco.Login();
+
+            Console.WriteLine("Init client...");
+            draco.Load();
+
+            Console.WriteLine("Get user items...");
+            var response = draco.Inventory.GetUserItems();
+            //for (const item of response.items) {
+            //    Console.WriteLine(`Item type ${ DracoNode.enums.ItemType[item.type]}, count = ${ item.count}`);
+            //}
+
+            Console.WriteLine("Done.");
         }
     }
 }
