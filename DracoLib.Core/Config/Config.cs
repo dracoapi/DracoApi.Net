b6141c273fc91e99488ba13e1d4d04e792d79a8d
Furtif <msntchat@hotmail.fr>
2018-05-27 22:08:13 +0200

    set defaults

--- DracoLib.Core/Config/Config.cs
+++ DracoLib.Core/Config/Config.cs
@@ -4,10 +4,10 @@ namespace DracoLib.Core
 {
     public class Config
     {
-        public bool CheckProtocol { get; set; }
+        public bool CheckProtocol { get; set; } = true;
         public Dictionary<string, int> EventsCounter { get; set; } = new Dictionary<string, int>();
-        public int UtcOffset { get; set; }
-        public int TimeOut { get; set; }
-        public string Lang { get; set; }
+        public int UtcOffset { get; set; } = 7200;
+        public int TimeOut { get; set; } = 0;
+        public string Lang { get; set; } = "English";
     }
 }
